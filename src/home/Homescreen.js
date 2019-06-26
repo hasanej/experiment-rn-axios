d7fe29cdeb4f4b414b8d456a4389c5c7967c8235
Hasan El Jabir <eljabirhasan@gmail.com>
2019-06-27 06:00:00 +0700

    Update base URL

--- src/home/Homescreen.js
+++ src/home/Homescreen.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 import ListItems from "./component/ListItems";
 
-const baseUrl = "http://192.168.43.161:5000/app";
+const baseUrl = "http://10.254.53.152:5000/app";
 
 //Realm
 var Realm = require('realm');
